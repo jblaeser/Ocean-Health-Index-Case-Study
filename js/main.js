87ce3bc998ef154eb4a0c519f45273faed343f6e
Jill Blaeser <jblaeser01@gmail.com>
2018-12-09 13:23:38 -0600

    Whoops Broke my JavaScript

--- js/main.js
+++ js/main.js
@@ -1,28 +1,24 @@
 $('.top-nav-list li').on('mouseover', function(){
-    $('.header-container').addClass('header-hover');
-  }).on('mouseout', function(){
-    $('.header-container').removeClass('header-hover');
-  })r
+  $('.header-container').addClass('header-hover');
+}).on('mouseout', function(){
+  $('.header-container').removeClass('header-hover');
+})
 
 
 
 var header = $('.header');
 $(window).on('scroll', function() {
-   var st = $(this).scrollTop();
-   header.css({ 'opacity' : (1 - st/150) });
+ var st = $(this).scrollTop();
+ header.css({ 'opacity' : (1 - st/150) });
 });
+
+
+
 
 
 var offset = $(".jellyfish").offset();
 $(window).scroll(function(event) {
-  var st = $(this).scrollTop();
-  $(".jellyfish").css("left", -st * 1.5 + offset.left);
-  $(".jellyfish").css("top", st * .5 + offset.top);
+var st = $(this).scrollTop();
+$(".jellyfish").css("left", -st * 1.5 + offset.left);
+$(".jellyfish").css("top", st * .5 + offset.top);
 });
-
-
-
-
-
-
-
