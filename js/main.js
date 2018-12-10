27e5c840927b9e8260772db15bc930d3b625867e
Jill Blaeser <jblaeser01@gmail.com>
2018-12-09 20:01:43 -0600

    Search Window

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $('.top-nav-list li').on('mouseover', function(){
 var header = $('.header');
 $(window).on('scroll', function() {
  var st = $(this).scrollTop();
- header.css({ 'opacity' : (1 - st/150) });
+ header.css({ 'opacity' : (1 - st/200) });
 });
 
 
@@ -21,4 +21,33 @@ $(window).scroll(function(event) {
 var st = $(this).scrollTop();
 $(".jellyfish").css("left", -st * 1.5 + offset.left);
 $(".jellyfish").css("top", st * .5 + offset.top);
+});
+
+
+var searchTop = document.querySelector('.search-top');
+searchTop.addEventListener('click', function() {console.log('Open Sesame');
+document.querySelector('html')
+    .classList
+    .toggle('search-open')
+});
+
+var searchBottom = document.querySelector('.search-bottom');
+searchBottom.addEventListener('click', function() {console.log('Open Sesame');
+document.querySelector('html')
+    .classList
+    .toggle('search-open')
+});
+
+var searchExit = document.querySelector('.exit-symbol');
+searchExit.addEventListener('click', function() {console.log('Close Sesame');
+document.querySelector('html')
+    .classList
+    .toggle('search-open')
+});
+
+var overlayExit = document.querySelector('.overlay');
+overlayExit.addEventListener('click', function() {console.log('Close Sesame');
+document.querySelector('html')
+    .classList
+    .toggle('search-open')
 });
